61c53c2048ce303efa6982d296666cf766e20b93
Maurizio Cardella <cardella.maurizio@gmail.com>
2017-11-28 13:16:10 +0100

    home module shell css

---
+++ home.js
@@ -0,0 +1,14 @@
+// Module variables
+const main_html = `
+<h1>HOME</>
+`
+// Export module initModule
+const initModule = ( container ) => {
+
+
+
+  container.innerHTML = main_html
+
+}
+
+export { initModule }
--- shell.js
+++ shell.js
@@ -1,43 +1,46 @@
+import * as modHome from '/home.js'
 // Module variables
 const main_html = `
-  <div class="spa-shell-head">
-    <div class="spa_shell_head_logo">
+  <div class="shell-head">
+    <div class="shell-head-logo">
       <h1>ES6</h1>
       <p>no framework</p>
     </div>
-    <div class="spa-shell-head-acct"></div>
+    <div class="shell-head-acct"></div>
   </div>
-  <div class="spa-shell-main">
-    <div class="spa-shell-main-nav"></div>
-    <div class="spa-shell-main-content">
-      <div class="spa-shell-main-content-message"></div>
-      <div class="spa-shell-main-content-body"></div>
+  <div class="shell-main">
+    <div class="shell-main-nav"></div>
+    <div class="shell-main-content">
+      <div class="shell-main-content-message"></div>
+      <div class="shell-main-content-body"></div>
     </div>
   </div>
-  <div class="spa-shell-foot"></div>
-  <div class="spa-shell-modal"></div>
+  <div class="shell-foot"></div>
+  <div class="shell-modal"></div>
 `
 const nav_html = `
   <ul>
-    <li id="home" class="spa-shell-nav-li"><a href="#home">Home</a></li>
-    <li id="news" class="spa-shell-nav-li"><a href="#news">News</a></li>
-    <li spa-shell-nav-li><a href="#contact">Contact</a></li>
-    <li spa-shell-nav-li><a href="#about">About</a></li>
+    <li id="home" class="shell-nav-li"><a href="#home">Home</a></li>
+    <li id="news" class="shell-nav-li"><a href="#news">News</a></li>
+    <li shell-nav-li><a href="#contact">Contact</a></li>
+    <li shell-nav-li><a href="#about">About</a></li>
   </ul>
 `
 const onClickMenuitem = ( id ) => {
     switch (id) {
-      case "home":
-        alert('HOME')
-        //spa.home.initModule( domMap.content );
+      case "home": {
+
+        modHome.initModule( document.querySelector('.shell-main-content') );
         break;
-      case "news":
+      }
+      case "news": {
         //spa.news.initModule( domMap.content );
         break;
-      case "nino":
-          //spa.nino.initModule( domMap.content );
-          break;
-
+      }
+      case "nino": {
+        //spa.nino.initModule( domMap.content );
+        break;
+      }
     }
 }
 // Export module initModule
@@ -45,17 +48,10 @@ const initModule = ( container ) => {
 
 
   container.innerHTML = main_html
-  document.querySelector('.spa-shell-main-nav').innerHTML = nav_html
+  document.querySelector('.shell-main-nav').innerHTML = nav_html
 
-  const li = document.getElementsByClassName( 'spa-shell-nav-li' )
-    for (let i=0; i < li.length; i++) {
+  const li = document.getElementsByClassName( 'shell-nav-li' )
 
-//        li[i].addEventListener(
-//          "click", function( event ) {
-//          onClickMenuitem( event.currentTarget.id )
-//          }
-//      )
-    }
   for (let _li of li) {
         _li.addEventListener(
           "click", ( event ) => {
