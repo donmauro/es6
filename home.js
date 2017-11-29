549930c0ce8054aba898b4db33a53e88ac53a3df
Maurizio Cardella <cardella.maurizio@gmail.com>
2017-11-29 19:02:36 +0100

    serialize array

--- home.js
+++ home.js
@@ -1,15 +1,44 @@
 import * as message from '/message.js'
 // Module variables
 const main_html = `
-<h1>HOME</>
+  <div>
+    <form>
+      Nome:<br>
+      <input type="text" name="nome"><br>
+      Cognome:<br>
+      <input type="text" name="cognome"><br>
+      Data:
+      <input type="date" name="dataNascita"><br>
+      <button type="button">Salva</button>
+    </form>
+  </div>
 `
+
+
+const serializeArray = ( fields ) => {
+  const object = {}
+  for( let field of fields ){
+    object[ field.name ] = field.value
+
+  }
+
+  return object
+}
 // Export module initModule
 const initModule = ( container ) => {
 
-
-
   container.innerHTML = main_html
 
+  document.querySelector('button')
+    .addEventListener('click', () => {
+      const elements = document.querySelectorAll('form input')
+
+      const dataObject = serializeArray( elements );
+      const jsonData = JSON.stringify(dataObject);
+      console.log( dataObject )
+      alert( jsonData )
+      //message.show('Salvato')
+    })
 }
 
 export { initModule }
