bdc9b96c9f2e782ec2905e4c42274f1f039c50f4
Maurizio Cardella <cardella.maurizio@gmail.com>
2018-01-08 14:14:19 +0100

    form elements

--- src/js/read.js
+++ src/js/read.js
@@ -12,29 +12,45 @@ Body module
 const main_html = `
   <div class="read">
     <form id="formUserSearch">
-      Nome: <input id="name" type="search" >
+      <label for='name'>Nome:<input id="name" type="search" ></input></label>
       <button id="search" type="submit">Trova</button>
       <br>
-      <input id="nuovo" type="button" value="Nuovo"></input><br>
-
     </form>
+    <input id="nuovo" type="button" value="Nuovo"></input><br>
     <div class="read-sub"></div>
   </div>
 `
 
 const create_html = `
   <div class="create">
-    <form id=formUserCreate>
+    <form id="formUserCreate">
       Nome:<br>
       <input type="text" name="nome"><br>
       Cognome:<br>
       <input type="text" name="cognome"><br>
       Data di nascita:<br>
       <input type="date" name="dataNascita"><br><br>
+      <p>Sesso</p>
+      <label for='male'>M:
+          <input type='radio' name='sesso' value='M' id='male'>
+      </label>
+      <label for='female'>F:
+          <input type='radio' name='sesso' value='F' id='female'>
+      </label>
+      <label for='citta'>Comune di nascita:
+        <select name='citta' id='citta'>
+          <option value='' selected>Scegli....</option>
+          <option value='Metropolis'>Palermo</option>
+          <option value='Gotham City'>Trapani</option>
+          <option value='Keystone City'>Agrigento</option>
 
+        </select>
+      </label>
+      <br>
+      <input type='submit'></input>
 
     </form>
-    <input name="salva" type="button" value="Salva"></input>
+
   </div>
 `
 /*
@@ -129,16 +145,32 @@ const serializeArray = ( fields ) => {
 
 const nuovo = () => {
   document.querySelector( '.read-sub' ).innerHTML = create_html
-  document.querySelector("input[name='salva']")
-    .addEventListener('click', () => {
+  const form = document.forms[ 'formUserCreate' ]
+  form.addEventListener( 'submit', ( event ) => {
+      event.preventDefault()
+      let jsonData = {}
+      console.log( form.elements )
+      for (const element  of form.elements) {
 
-      const elements = document.querySelectorAll('form input')
-      const dataObject = serializeArray( elements )
-      user.create( dataObject )
 
+          jsonData[ element.name ] = element.value;
+      }
+      console.log( jsonData )
+      user.create(  jsonData  )
+
+
+
+
+//  document.querySelector("input[name='salva']")
+//    .addEventListener('click', () => {
+//
+//      const elements = document.querySelectorAll('form input')
+//      const dataObject = serializeArray( elements )
+//      user.create( dataObject )
+//
+//  })
   })
 }
-
 // Export module initModule
 const initModule = ( container ) => {
 
@@ -146,8 +178,7 @@ const initModule = ( container ) => {
   const form = document.forms.formUserSearch
   form.addEventListener ('submit', search, false);
 
-  document.querySelector("input[id='nuovo']")
-  .addEventListener('click', nuovo )
+  document.querySelector("input[id='nuovo']").addEventListener('click', nuovo )
 
 }
 
